1695484f2de2d6c3f7e229b7c1d5f6a3c8e24874
sohil5220 <monparasohil@gmail.com>
2024-11-30 16:29:52 +0530

    final changes

--- React/src/components/team/App.js
+++ React/src/components/team/App.js
@@ -50,7 +50,7 @@ class App extends Component {
 											<h3>atrivedi [AT] iitg.ac.in</h3>
 										</div>
 										<div class="team-img">
-											<img src={require("./img/aditya.jpg")} alt="Team Image" />
+											<img src={require("./img/team-1-1.jpg")} alt="Team Image" />
 											<div class="team-content">
 												<p>Primarily worked on building React based Frontend and integrated the Django backend</p>
 											</div>
@@ -58,10 +58,10 @@ class App extends Component {
 										<div class="team-content">
 											<div class="team-social">
 
-												<a class="social-fb" href="https://www.facebook.com/aditya.trivedi.9028/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
-												<a class="social-li" href="https://www.linkedin.com/in/trivedi-aditya/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-												<a class="social-in" href="https://www.instagram.com/adityatrivediii/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
-												<a class="social-gi" href="https://github.com/ad1tyat" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+												<a class="social-fb" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+												<a class="social-li" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+												<a class="social-in" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+												<a class="social-gi" href="h" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
 
 											</div>
 										</div>
@@ -74,7 +74,7 @@ class App extends Component {
 											<h3>v.atharva [AT] iitg.ac.in</h3>
 										</div>
 										<div class="team-img">
-											<img src={require("./img/atharva.jpg")} alt="Team Image" />
+											<img src={require("./img/team-1-2.jpg")} alt="Team Image" />
 											<div class="team-content">
 												<p>Worked on collection of data and built the Neo4J database</p>
 											</div>
@@ -82,10 +82,10 @@ class App extends Component {
 										<div class="team-content">
 											<div class="team-social">
 
-												<a class="social-fb" href="https://www.facebook.com/profile.php?id=100009062382260" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
-												<a class="social-li" href="https://www.linkedin.com/in/atharva-varde-643a05197/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-												<a class="social-in" href="https://www.instagram.com/kinginthenorth_561/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
-												<a class="social-gi" href="https://github.com/varde2407" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+												<a class="social-fb" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+												<a class="social-li" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+												<a class="social-in" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+												<a class="social-gi" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
 											</div>
 										</div>
 									</div>
@@ -93,11 +93,11 @@ class App extends Component {
 								<div class="column">
 									<div class="team-4">
 										<div class="team-content">
-											<h2>Shashwat Sharma</h2>
-											<h3>shashwat.sharma [AT] iitg.ac.in</h3>
+											<h2>Sohil Monpara</h2>
+											<h3>m.sohil [AT] iitg.ac.in</h3>
 										</div>
 										<div class="team-img">
-											<img src={require("./img/shash.png")} alt="Team Image" />
+											<img src={require("./img/team-1-3.png")} alt="Team Image" />
 											<div class="team-content">
 												<p>Optimised the scraping and worked on Django-Neo4J integration</p>
 											</div>
@@ -105,10 +105,10 @@ class App extends Component {
 										<div class="team-content">
 											<div class="team-social">
 
-												<a class="social-fb" href="https://www.facebook.com/shashwat.sharma.5209" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+												<a class="social-fb" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
 												<a class="social-li" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-												<a class="social-in" href="https://www.instagram.com/watshashwat/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
-												<a class="social-gi" href="https://github.com/Svanazar" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+												<a class="social-in" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+												<a class="social-gi" href="" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
 											</div>
 										</div>
 									</div>
